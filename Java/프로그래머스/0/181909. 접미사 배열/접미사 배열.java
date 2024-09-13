871c79c3f47e739d6d3a98e88d84408c4111108d
dhlee47-l <65275537+dhlee47-l@users.noreply.github.com>
2024-09-13 11:39:58 +0900

    [level 0] Title: 접미사 배열, Time: 1.10 ms, Memory: 73.7 MB -BaekjoonHub

--- Java/프로그래머스/0/181909. 접미사 배열/접미사 배열.java
+++ Java/프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -1,17 +1,36 @@
+//내 풀이:
+//import java.util.Arrays;
+// class Solution {
+//     public String[] solution(String my_string) {
+//         int n = my_string.length();
+//         String[] answer = new String [n];
+//         String my_reversed = new StringBuilder(my_string).reverse().toString();
+//         //StringBuilder()
+//         //reverse().toString();
+        
+//         for(int i=0;i<n;i++){
+//             String substring=my_reversed.substring(0,i+1);
+//             answer[i] = new StringBuilder(substring).reverse().toString();
+//         }
+        
+//         Arrays.sort(answer);
+//         return answer;
+//     }
+// }
+
+
+//다른 사람 풀이:
 import java.util.Arrays;
+
 class Solution {
+
     public String[] solution(String my_string) {
-        int n = my_string.length();
-        String[] answer = new String [n];
-        String my_reversed = new StringBuilder(my_string).reverse().toString();
-        //StringBuilder()
-        //reverse().toString();
-        
-        for(int i=0;i<n;i++){
-            String substring=my_reversed.substring(0,i+1);
-            answer[i] = new StringBuilder(substring).reverse().toString();
+        String[] answer = new String[my_string.length()];
+
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = my_string.substring(i, my_string.length());
         }
-        
+
         Arrays.sort(answer);
         return answer;
     }
