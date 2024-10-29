b4b182da6d112fb5f244bef6c2cd98d2992665dc
Donghee Lee <65275537+dhlee47-l@users.noreply.github.com>
2024-10-29 15:55:38 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    
+    numArr=n.toString().split('');
+    
+    numArr.sort((a, b) => b - a);
+    
+    let answer = parseInt(numArr.join(''), 10);
+    
+    return answer;
+}
+
